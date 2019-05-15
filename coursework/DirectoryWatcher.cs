f7c8f7c38b89bc4002f3fae5929805ee96a09e6d
Astreb <39983706+Astreb@users.noreply.github.com>
2019-05-15 07:25:27 +0300

    Added event handlers. Created class DirectoryWatcher

---
+++ coursework/DirectoryWatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursework
+{
+    class DirectoryWatcher
+    {
+        string directoryPath;
+        MainWindow window;
+
+        public DirectoryWatcher(MainWindow window, string directoryPath)
+        {
+            this.directoryPath = directoryPath;
+            this.window = window;
+        }
+
+        public void PrintChangesLog()
+        {
+
+        }
+    }
+}
--- coursework/MainWindow.xaml.cs
+++ coursework/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace coursework
 {
@@ -20,9 +22,54 @@ namespace coursework
     /// </summary>
     public partial class MainWindow : Window
     {
+        DirectoryWatcher watcher;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.watcher = null;
+        }
+
+        private void DirectoryButtonClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                this.directoryPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void RunButtonClick(object sender, RoutedEventArgs e)
+        {
+            bool status = true;
+            if (this.runButton.Content.ToString() == "Включить мониторинг")
+                status = true;
+            else
+                status = false;
+
+            if (!status)
+            {
+                this.runButton.Content = "Включить мониторинг";
+                this.changesLog.Items.Add("Работа завершена.");
+                this.watcher = null;
+            }
+            else
+            {
+                string directoryPath = this.directoryPath.Text;
+                bool folderExists = Directory.Exists(directoryPath);
+
+                if (!folderExists)
+                {
+                    System.Windows.Forms.MessageBox.Show("Выбранная директория не существует!",
+                                                "Мониторинг", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                this.watcher = new DirectoryWatcher(this, directoryPath);
+                this.runButton.Content = "Завершить мониторинг";
+            }
         }
     }
 }
